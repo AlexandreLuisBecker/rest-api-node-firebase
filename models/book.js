c7ae9d441175957a9c16ed1065049c477cfbe197
AlexandreLuisBecker <didiao@yahoo.com>
2023-08-14 20:24:30 -0300

    Criação do model Book

---
+++ models/book.js
@@ -0,0 +1,17 @@
+// Definindo a classe 'Book'
+class Book {
+    // Implementando o 'construtor da classe'
+    constructor (
+        id,
+        titulo,
+        tituloOriginal,
+        genero,
+        anoLancamento
+    ) {
+        this.id = id
+        this.titulo = titulo,
+        this.tituloOriginal = tituloOriginal,
+        this.genero = genero,
+        this.anoLancamento = anoLancamento
+    }
+}
